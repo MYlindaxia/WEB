edc8d584e8470065d06d51a61c9c75c2d68506f5
MYlindaxia <2444501319@qq.com>
2020-08-24 19:59:45 +0800

    220200824更新

--- 利用js实现二级下拉菜单/index.js
+++ 利用js实现二级下拉菜单/index.js
@@ -1,24 +1,19 @@
-// window.onload = function(){
-//     var oneMenu = document.getElementById("li-one");
-// };
-
 window.onload = function(){
-	//获取所有的一级菜单
 	var oneMenu = document.getElementsByClassName("li-one");
-	
 	for(var i = 0;i<oneMenu.length;i++){
 		oneMenu[i].onmouseover = function(){
-			var twoMenu = this.getElementsByTagName("ul");
+			var twoMenu = document.getElementsByTagName("ul");
 			if(twoMenu.length != 0){
 				this.getElementsByTagName("ul")[0].style.display = "block";
-				this.getElementsByTagName("ul")[0].style.backgroundColor = "blueviolet";
+				this.getElementsByTagName("ul")[0].style.backgroundColor = "red";
 			}
 		};
 		oneMenu[i].onmouseout = function(){
 			var twoMenu = this.getElementsByTagName("ul");
 			if(twoMenu.length != 0){
-				this.getElementsByTagName("ul")[0].style.display = "none";
+				this.getElementsByTagName("ul")[0].style = "none";
 			}
 		};
 	}	
 };
+
